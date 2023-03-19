5534bdab9aaa53addf30570d82d132381391d16d
smtronic <aasmolnikov@gmail.com>
2023-03-19 16:12:54 +0300

    added files to gitignore.

--- Python_parse/task1.5.py
+++
@@ -1,5 +0,0 @@
-from bs4 import BeautifulSoup
-from urllib.request import urlopen
-html = urlopen('https://stepik.org/media/attachments/lesson/209723/5.html').read().decode('utf8')# скачиваем файл
-soup = BeautifulSoup(html, 'html.parser')
-print(sum(int(i.get_text()) for i in soup.find_all('td')))
