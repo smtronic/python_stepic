6ea4816d74c9ed245f292e2b3dec3da2f757943a
smtronic <aasmolnikov@gmail.com>
2023-03-23 19:12:34 +0300

    deleted folder dict

--- python_stepic/dict/chart 10_3/task_12.py
+++
@@ -1,8 +0,0 @@
-#  переменной result хранился словарь,
-#  в котором для каждого символа строки text будет подсчитано количество его вхождений.
-text = 'footballcyberpunkextraterritorialityconversationalistblockophthalmoscopicinterdependencemamauserfff'
-# result = {}
-# for c in text:
-#     result[c] = result.get(c, 0) + 1
-
-result = {char: text.count(char) for char in set(text)}
--- python_stepic/dict/chart 10_3/task_13.py
+++
@@ -1,9 +0,0 @@
-# код, чтобы он вывел наиболее часто встречающееся слово строки s.
-# Если таких слов несколько, должно быть выведено то, что меньше в лексикографическом порядке.
-s = 'orange strawberry barley gooseberry apple apricot barley currant orange melon pomegranate banana banana orange barley apricot plum grapefruit banana quince strawberry barley grapefruit banana grapes melon strawberry apricot currant currant gooseberry raspberry apricot currant orange lime quince grapefruit barley banana melon pomegranate barley banana orange barley apricot plum banana quince lime grapefruit strawberry gooseberry apple barley apricot currant orange melon pomegranate banana banana orange apricot barley plum banana grapefruit banana quince currant orange melon pomegranate barley plum banana quince barley lime grapefruit pomegranate barley'
-result = {c: s.count(c) for c in set(s.split())}
-
-for key, item in sorted(result.items()):
-    if item == max(result.values()):
-        print(key)
-        break
